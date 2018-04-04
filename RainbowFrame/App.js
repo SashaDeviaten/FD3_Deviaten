3ca51443c82724fef598a9c1bd7096705a86e16e
Девятень Александр <aleksandr.devyaten@service-it.by>
2018-04-04 10:25:25 +0300

    add RainbowFrame first version

---
+++ RainbowFrame/App.js
@@ -0,0 +1,17 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ColorFrames from './components/ColorFrames';
+
+let something = 'Что-нибудь';
+
+let colors = ['red', 'blue', 'green', 'black', 'yellow'];
+
+ReactDOM.render(
+    <ColorFrames colors={colors}
+                 content={something}/>
+    , document.getElementById('container')
+);
+
---
+++ RainbowFrame/components/ColorFrames.js
@@ -0,0 +1,37 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+
+export default class ColorFrames extends React.Component {
+
+    static propTypes = {
+        colors: PropTypes.arrayOf(
+            PropTypes.string.isRequired),
+    };
+    state = {
+        colors: this.props.colors.slice()
+    };
+
+
+    render() {
+        let frame;
+        if (this.state.colors.length) {
+            frame = <div style={{border: "solid 2px " + this.state.colors.shift(), padding: "5px"}}>
+                <ColorFrames colors={this.state.colors} content={this.props.content}/>
+            </div>
+        }
+        else {
+            frame = <div>
+                {this.props.content}
+            </div>
+
+        }
+        return (
+            <div>
+                {frame}
+            </div>
+        );
+    }
+}
+
+
