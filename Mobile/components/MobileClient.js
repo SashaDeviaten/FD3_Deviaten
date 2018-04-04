a437016a52f96843f4b00a73bdf84f6d392dbca0
Девятень Александр <aleksandr.devyaten@service-it.by>
2018-04-04 18:30:05 +0300

    add Mobile first version

---
+++ Mobile/components/MobileClient.js
@@ -0,0 +1,105 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './MobileClient.css';
+
+export default class MobileClient extends React.PureComponent {
+
+    static propTypes = {
+        info: PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            fio: PropTypes.string.isRequired,
+            balance: PropTypes.number.isRequired,
+        }),
+        cbDeleteClient: PropTypes.func.isRequired,
+        cbChangeClientFio: PropTypes.func.isRequired,
+        cbChangeClientBalance: PropTypes.func.isRequired
+
+    };
+
+    state = {
+        info: {...this.props.info},
+        showButtons: false,
+        showFioInput: false,
+        showBalanceInput: false,
+    };
+
+    showButtons = () => {
+        this.setState({showButtons: !this.state.showButtons})
+    };
+
+    deleteClient = () => {
+        this.props.cbDeleteClient(this.state.info.id)
+    };
+
+    changeFio = () => {
+        this.setState({showFioInput: false});
+        if (this.newFioRef) {
+            let newFio = this.newFioRef.value;
+            this.props.cbChangeClientFio(this.state.info.id, newFio)
+        }
+    };
+
+    changeBalance = () => {
+        this.setState({showBalanceInput: false});
+        if (this.newBalanceRef) {
+            let newBalance = this.newBalanceRef.value;
+            this.props.cbChangeClientBalance(this.state.info.id, newBalance)
+        }
+    };
+
+    showFioInput = () => {
+        this.showButtons();
+        this.setState({showFioInput: true})
+    };
+
+    showBalanceInput = () => {
+        this.showButtons();
+        this.setState({showBalanceInput: true})
+    };
+
+    setNewFioRef = (ref) => {
+        this.newFioRef = ref;
+    };
+
+    setNewBalanceRef = (ref) => {
+        this.newBalanceRef = ref;
+    };
+
+    render() {
+
+        console.log("MobileClient id=" + this.state.info.id + " render");
+
+        return (
+            <div className='MobileClient'>
+                <div>
+                    <span className='MobileClientFIO' onClick={this.showButtons}>{this.props.info.fio}</span>
+                    <span className='MobileClientBalance'>{this.props.info.balance}</span>
+                </div>
+                {
+                    (this.state.showButtons) && <div>
+                        <input type='button' value='Удалить' onClick={this.deleteClient}/>
+                        <input type='button' value='Изменить фамилию' onClick={this.showFioInput}/>
+                        <input type='button' value='Изменить баланс' onClick={this.showBalanceInput}/>
+                        <input type='button' value='Скрыть панель изменений' onClick={this.showButtons}/>
+                    </div>
+                }
+                {
+                    (this.state.showFioInput) && <div>
+                        <input type='text' placeholder='Измененные ФИО' ref={this.setNewFioRef}/>
+                        <input type='button' value='Сохранить изменение' onClick={this.changeFio}/>
+                    </div>
+                }
+                {
+                    (this.state.showBalanceInput) && <div>
+                        <input type='text' placeholder='Измененный баланс' ref={this.setNewBalanceRef}/>
+                        <input type='button' value='Сохранить изменение' onClick={this.changeBalance}/>
+                    </div>
+                }
+            </div>
+        );
+
+    }
+
+}
+
---
+++ Mobile/components/MobileCompany.js
@@ -0,0 +1,133 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import MobileClient from './MobileClient';
+
+import './MobileCompany.css';
+
+export default class MobileCompany extends React.PureComponent {
+
+    static propTypes = {
+        name: PropTypes.string.isRequired,
+        clients: PropTypes.arrayOf(
+            PropTypes.shape({
+                id: PropTypes.number.isRequired,
+                fio: PropTypes.string.isRequired,
+                balance: PropTypes.number.isRequired,
+            })
+        ),
+    };
+
+    state = {
+        name: this.props.name,
+        clients: this.props.clients,
+        showNewClientCard:false,
+    };
+
+    setName1 = () => {
+        this.setState({name: 'МТС'});
+    };
+
+    setName2 = () => {
+        this.setState({name: 'Velcom'});
+    };
+
+
+    addClient = () => {
+        this.state.showNewClientCard=false;
+        let newClients = [...this.state.clients];
+        let newClient={};
+        newClient.id=this.state.clients.length+1;
+        newClient.fio=this.newClientFioRef.value;
+        newClient.balance=parseInt(this.newClientBalanceRef.value);
+        newClients.push(newClient);
+        this.setState({clients: newClients});
+    };
+
+    showNewClientCard = () => {
+        this.setState({showNewClientCard: true})
+    };
+
+    deleteClient = (clientId) => {
+        let newClients = this.state.clients.filter((c) => {
+            return (c.id !== clientId)
+        });
+        this.setState({clients: newClients});
+    };
+    changeClientFio = (clientId,newFio) => {
+        let newClients = [...this.state.clients];
+        newClients.forEach((c,i) => {
+            if (c.id === clientId) {
+                let newClient = {...c};
+                newClient.fio = newFio;
+                newClients[i] = newClient;
+            }
+        });
+        this.setState({clients: newClients});
+    };
+    changeClientBalance = (clientId,newBalance) => {
+        let newClients = [...this.state.clients];
+        newClients.forEach((c,i) => {
+            if (c.id === clientId) {
+                let newClient = {...c};
+                newClient.balance = parseInt(newBalance);
+                newClients[i] = newClient;
+            }
+        });
+        this.setState({clients: newClients});
+    };
+
+    setNewClientFioRef = (ref) => {
+        this.newClientFioRef = ref;
+    };
+
+    setNewClientBalanceRef = (ref) => {
+        this.newClientBalanceRef = ref;
+    };
+
+    filterByBalance = () => {
+        let newClients = this.state.clients.sort((a,b) => {
+            return (b.balance-a.balance)
+        });
+        this.setState({clients: newClients});
+    };
+
+    render() {
+        console.log("MobileCompany render");
+
+        let clientsCode = this.state.clients.map(client =>
+            <MobileClient key={client.id}
+                          info={client}
+                          cbDeleteClient={this.deleteClient}
+                          cbChangeClientFio={this.changeClientFio}
+                          cbChangeClientBalance={this.changeClientBalance}/>
+        );
+
+
+        return (
+            <div className='MobileCompany'>
+                <input type="button" value="МТС" onClick={this.setName1}/>
+                <input type="button" value="Velcom" onClick={this.setName2}/>
+                <div className='MobileCompanyName'>Компания "{this.state.name}"</div>
+                <div className='MobileCompanyClients'>
+                    {clientsCode}
+                </div>
+                <input type="button" value="Добавить клиента" onClick={this.showNewClientCard}/>
+                {
+                    (this.state.showNewClientCard) && <div>
+                        <input type='text' placeholder='ФИО' ref={this.setNewClientFioRef}/>
+                        <input type='text' placeholder='Баланс' ref={this.setNewClientBalanceRef}/>
+                        <input type='button' value='Сохранить клиента' onClick={this.addClient}/>
+                    </div>
+                }
+                <input type="button" value="Отфильтровать по балансу" onClick={this.filterByBalance}/>
+                <input type="button" value="Отфильтровать активных" onClick={this.showNewClientCard}/>
+                <input type="button" value="Отфильтровать должников" onClick={this.showNewClientCard}/>
+            </div>
+        )
+            ;
+
+    }
+
+}
+
