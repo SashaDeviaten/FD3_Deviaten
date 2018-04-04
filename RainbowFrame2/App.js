630958e633d235197c6f1a6bded05bba2811ba62
Девятень Александр <aleksandr.devyaten@service-it.by>
2018-04-04 10:57:37 +0300

    add RainbowFrame2 second version

---
+++ RainbowFrame2/App.js
@@ -0,0 +1,18 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ColorFrames from './components/ColorFrames';
+
+let something = 'Что-нибудь';
+
+let colors = ['red', 'blue', 'green'];
+
+ReactDOM.render(
+    <ColorFrames colors={colors}>
+        {something}
+    </ColorFrames>
+    , document.getElementById('container')
+);
+
---
+++ RainbowFrame2/components/ColorFrames.js
@@ -0,0 +1,40 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+
+export default class ColorFrames extends React.Component {
+
+    static propTypes = {
+        colors: PropTypes.arrayOf(
+            PropTypes.string.isRequired),
+    };
+    state = {
+        colors: this.props.colors.slice(),
+        content: this.props.children
+    };
+
+
+    render() {
+        let frame;
+
+        (this.state.colors.length
+                ? frame = <div style={{border: "solid 2px " + this.state.colors.shift(), padding: "5px"}}>
+                    <ColorFrames colors={this.state.colors}>
+                        {this.state.content}
+                    </ColorFrames>
+                </div>
+                : frame = <div style={{border: "solid 2px " + this.state.colors.shift(), padding: "5px"}}>
+                    {this.state.content}
+                </div>
+        );
+
+        return (
+            <div>
+                {frame}
+            </div>
+
+        );
+    }
+}
+
+
